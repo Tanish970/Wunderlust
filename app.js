f1168ae9b07aae869e7757ff5d1bbd324ea51883
Tanish970 <rohith.guntur@gmail.com>
2024-04-24 03:40:08 +0000

    signup page

--- app.js
+++ app.js
@@ -4,32 +4,76 @@ const path = require('path');
 const Listing = require('/workspaces/Wunderlust/Models/listings.js'); // Adjust path as needed
 const app = express();
 const exec = require('child_process').exec;
-
+const passport = require('passport');
+const localStrategy = require("passport-local");
 const port = 8081;
-
 const methodOverride = require('method-override');
 const engine = require('ejs-mate');
+const session = require('express-session');
+const User = require('./Models/user.js');
+const flash = require('connect-flash');
+
+const crypto = require('crypto');
+const secretKey = crypto.randomBytes(32).toString('hex');
 
 // Replace with your MongoDB Atlas connection string
 mongoose.connect('mongodb+srv://rohithguntur:test123@cluster0.3xgqumk.mongodb.net/wanderlust?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('Connected to MongoDB Atlas and using the wanderlust database!'))
   .catch(err => console.error('Could not connect to MongoDB Atlas.', err));
 
-
 // Set views and view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.engine('ejs', engine);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
 
+// Set up session
+app.use(session({
+  secret: secretKey,
+  resave: false,
+  saveUninitialized: true
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/listings",async (req,res)=>{
     const allListing=await Listing.find({});
     res.render('../views/listings/index.ejs', { allListing });
         
+})
+app.get("/signup", (req, res) => {
+  res.render('../views/users/signup.ejs');
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
+    const newUser = new User({ email, username });
+    const registerUser = await User.register(newUser, password);
+    console.log(registerUser);
+    req.flash("success", "Welcome to Wanderlust");
+    res.redirect("/listings");
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
+});
+
+app.get("/demouser", async(req,res)=>{
+  const fakeUser = new User({
+    email:"student@gmail.com",
+    username:"delta-student",
+  })
+  let registeredUser=await User.register(fakeUser,"helloworld")
+  res.send(registeredUser)
 })
 
 
